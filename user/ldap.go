d4f936a36583a676def0e379a1c58668dd0a71de
Dmitry Baryshnikov <bishop.dev@gmail.com>
2020-09-20 01:05:53 +0300

    Add headers.

--- user/ldap.go
+++ user/ldap.go
@@ -1,3 +1,29 @@
+/*
+ * File: ldap.go
+ * Project: ngcommon
+ * File Created: Wednesday, 27th May 2019 12:54:33 am
+ * Author: Dmitry Baryshnikov <dmitry.baryshnikov@nextgis.com>
+ * -----
+ * Last Modified: Sunday, 20th September 2020 1:03:33 am
+ * Modified By: Dmitry Baryshnikov, <dmitry.baryshnikov@nextgis.com>
+ * -----
+ * Copyright 2019 - 2020 NextGIS, <info@nextgis.com>
+ * 
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *   This program is distributed in the hope that it will be useful
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
 package users
 
 import (
@@ -15,7 +41,6 @@ import (
 	"github.com/go-ldap/ldap/v3"
 
 	"github.com/nextgis/commons-go/context"
-	sentrygin "github.com/getsentry/sentry-go/gin"
 )
 
 func searchLDAPUser(username string, conn *ldap.Conn) (*ldap.Entry, error) {
--- util/util.go
+++ util/util.go
@@ -1,3 +1,29 @@
+/*
+ * File: util.go
+ * Project: ngcommon
+ * File Created: Tuesday, 26th May 2019 6:30:27 pm
+ * Author: Dmitry Baryshnikov <dmitry.baryshnikov@nextgis.com>
+ * -----
+ * Last Modified: Sunday, 20th September 2020 1:04:44 am
+ * Modified By: Dmitry Baryshnikov, <dmitry.baryshnikov@nextgis.com>
+ * -----
+ * Copyright 2019 - 2020 NextGIS, <info@nextgis.com>
+ * 
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *   This program is distributed in the hope that it will be useful
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
 package util
 
 import (
