929eb0c49a5a2053f1e0f83b7b6fb80399992325
Dmitry Baryshnikov <bishop.dev@gmail.com>
2021-04-08 15:09:14 +0300

    Add etag generation

---
+++ util/etag.go
@@ -0,0 +1,33 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2017 Amal Francis
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// Got from https://github.com/amalfra/etag
+
+package util
+
+import (
+	"crypto/sha1"
+	"fmt"
+)
+
+func getHash(str string) string {
+	return fmt.Sprintf("%x", sha1.Sum([]byte(str)))
+}
+
+// Generates an Etag for given string. Allows specifying whether to generate weak
+// Etag or not as second parameter
+func GenerateETag(str string, weak bool) string {
+	tag := fmt.Sprintf("\"%d-%s\"", len(str), getHash(str))
+	if weak {
+		tag = "W/" + tag
+	}
+
+	return tag
+}
